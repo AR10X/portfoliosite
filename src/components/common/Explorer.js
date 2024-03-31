7a3e60920583690001b64c4b87e528f0dfdad1b7
AR10X <arehamanofficial@gmail.com>
2024-03-31 17:13:28 +0530

    icons added

--- src/components/common/Explorer.js
+++ src/components/common/Explorer.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import '../../assets/styles/common/Explorer.css';
 import {Link} from 'react-router-dom';
-
+import jsIcon from '../../assets/images/js-icon.jpg';
+import pugIcon from '../../assets/images/pug-icon.jpg';
+import cssIcon from '../../assets/images/css-icon.jpg';
+import jsonIcon from '../../assets/images/json-icon.jpg';
 
 
 const Explorer = () => {
     return (
       <div className='explorer'>
+        <p className='heading-lg'>EXPLORER</p>
+        <p className='heading-sm'>PORTFOLIO-APP</p>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to='/'><img src={jsIcon} alt='jsIcon' />Home</Link>
           </li>
           <li>
-            <Link to='/about'>About</Link>
+            <Link to='/about'><img src={pugIcon} alt='pugIcon' />About</Link>
           </li>
           <li>
-            <Link to='/Projects'>Projects</Link>
+            <Link to='/Projects'><img src={cssIcon} alt='cssIcon' />Projects</Link>
           </li>
           <li>
-            <Link to='/Contact'>Contact</Link>
+            <Link to='/Contact'><img src={jsonIcon} alt='jsonIcon' />Contact</Link>
           </li>
         </ul>
       </div>
