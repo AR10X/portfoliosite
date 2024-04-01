d7c88faccba2eccc767e699a16132d8c8149d3aa
AR10X <arehamanofficial@gmail.com>
2024-04-01 14:16:26 +0530

    Before: tabbar state lifting

--- src/components/common/Explorer.js
+++ src/components/common/Explorer.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import '../../assets/styles/common/Explorer.css';
 import {Link} from 'react-router-dom';
-import jsIcon from '../../assets/images/js-icon.jpg';
-import pugIcon from '../../assets/images/pug-icon.jpg';
-import cssIcon from '../../assets/images/css-icon.jpg';
-import jsonIcon from '../../assets/images/json-icon.jpg';
+import jsIcon from '../../assets/images/tech-icons/js-icon-new.jpg';
+import pugIcon from '../../assets/images/tech-icons/pug-new.jpg';
+import cssIcon from '../../assets/images/tech-icons/css-new.jpg';
+import jsonIcon from '../../assets/images/tech-icons/json-new.jpg';
 
 
 const Explorer = () => {
-    return (
+  const [isOpen, setIsOpen] = useState(true);
+  
+  const toggleOptions = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
       <div className='explorer'>
-        <p className='heading-lg'>EXPLORER</p>
-        <p className='heading-sm'>PORTFOLIO-APP</p>
+        <p  className='heading-1' >EXPLORER</p>
+        <p className='heading-2' onClick={toggleOptions} >{isOpen ? (
+          <FontAwesomeIcon icon={faChevronDown} />
+        ) : (
+          <FontAwesomeIcon icon={faChevronRight} />
+        )}<span className='span-space'></span>PORTFOLIO-APP
+        </p>
+        {isOpen && (
         <ul>
           <li>
-            <Link to='/'><img src={jsIcon} alt='jsIcon' />Home</Link>
+            <Link to='/'><img src={jsIcon} alt='jsIcon' /><span className='span-space'></span>Home.js</Link>
           </li>
           <li>
-            <Link to='/about'><img src={pugIcon} alt='pugIcon' />About</Link>
+            <Link to='/about'><img src={pugIcon} alt='pugIcon' /><span className='span-space'></span>About.pug</Link>
           </li>
           <li>
-            <Link to='/Projects'><img src={cssIcon} alt='cssIcon' />Projects</Link>
+            <Link to='/Projects'><img src={cssIcon} alt='cssIcon' /><span className='span-space'></span>Projects.css</Link>
           </li>
           <li>
-            <Link to='/Contact'><img src={jsonIcon} alt='jsonIcon' />Contact</Link>
+            <Link to='/Contact'><img src={jsonIcon} alt='jsonIcon' /><span className='span-space'></span>Contact.json</Link>
           </li>
         </ul>
+        )}
       </div>
     );
 };
--- src/components/common/TabBar.js
+++ src/components/common/TabBar.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../../assets/styles/common/TabBar.css';
 import {Link} from 'react-router-dom';
-
+import jsIcon from '../../assets/images/tech-icons/js-icon-new.jpg';
+import pugIcon from '../../assets/images/tech-icons/pug-new.jpg';
+import cssIcon from '../../assets/images/tech-icons/css-new.jpg';
+import jsonIcon from '../../assets/images/tech-icons/json-new.jpg';
 
 
 const TabBar = () => {
@@ -9,16 +12,16 @@ const TabBar = () => {
       <div className='tabbar'>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to='/'><img src={jsIcon} alt='jsIcon' /><span className='span-space'></span>Home.js</Link>
           </li>
           <li>
-            <Link to='/about'>About</Link>
+            <Link to='/about'><img src={pugIcon} alt='pugIcon' /><span className='span-space'></span>About.pug</Link>
           </li>
           <li>
-            <Link to='/Projects'>Projects</Link>
+            <Link to='/Projects'><img src={cssIcon} alt='cssIcon' /><span className='span-space'></span>Projects.css</Link>
           </li>
           <li>
-            <Link to='/Contact'>Contact</Link>
+            <Link to='/Contact'><img src={jsonIcon} alt='jsonIcon' /><span className='span-space'></span>Contact.json</Link>
           </li>
         </ul>
       </div>
