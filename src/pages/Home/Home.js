036b42ab511b14cf9a154bb06d1559481f2c5c29
AR10X <arehamanofficial@gmail.com>
2024-04-04 21:57:52 +0530

    Done: home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,76 @@
 import React from 'react';
+import NumImg from '../../assets/images/home/numbers.jpg';
+import './Home.css';
+import Illust from '../../assets/images/home/Group.svg';
+import styled from 'styled-components';
 
 
+const BlueText = styled.span`
+  color: #007ACC;
+`;
 
+const GreenText = styled.span`
+  color: #6A8A35;
+`;
+
+const YellowText = styled.span`
+  color: #FFD710;
+`;
+const WhiteText = styled.span`
+  color: #ffffff;
+`;
 
 const Home = () => {
-    return (
-      <div >
-        <h1>Home</h1>
+  return (
+      <div className='home-main'>
+        <div>
+          <img src={NumImg} alt='numbers'></img>
+        </div>
+        <div className='home-left'>
+          <div className='home-txt-1'>
+            <BlueText>const </BlueText>
+            <WhiteText>express </WhiteText>
+            <YellowText>= </YellowText>
+            <BlueText>require</BlueText>
+            <YellowText>(</YellowText>
+            <WhiteText>'express'</WhiteText>
+            <YellowText>)</YellowText>
+            <YellowText>;</YellowText>
+          </div>
+          <div className='home-txt-2'>
+            <BlueText>const </BlueText>
+            <WhiteText>app </WhiteText>
+            <YellowText>= </YellowText>
+            <WhiteText>express</WhiteText>
+            <YellowText>(</YellowText>
+            <YellowText>)</YellowText>
+            <YellowText>;</YellowText>
+          </div>
+          <div className='home-txt-3'>
+            <WhiteText>app.use</WhiteText>
+            <YellowText>(</YellowText>
+            <BlueText>function( </BlueText>
+            <WhiteText>req, res</WhiteText>
+            <BlueText>){'{'}</BlueText>
+          </div>
+          <div className='home-block'>
+            <div className='hm-green'>
+              <GreenText>// Hello! I am</GreenText>
+            </div>
+            <h1>Abdul Rehaman</h1>
+            <div className='hm-green'>
+              <GreenText>// I am a Backend Engineer.</GreenText>
+            </div>
+          </div>
+          <div className='home-txt-4'>
+            <BlueText>{'}'}</BlueText>
+            <YellowText>)</YellowText>
+            <WhiteText>;</WhiteText>
+          </div>
+        </div>
+        <div className='home-right'>
+          <img src={Illust} alt='illustration'></img>
+        </div>
       </div>
     );
 };
